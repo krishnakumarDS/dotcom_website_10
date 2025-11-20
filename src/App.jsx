d29f13e24fd59656221b9222f34ef17443135083
krishnakumarDS <krishnakumarDS@users.noreply.github.com>
2025-11-20 21:34:21 +0530

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
